6ad0c792d942a5ab2fe2787cc7ec9c53fa6ccd4b
Viktoria Radchenko <viktoriia.rad@gmail.com>
2019-05-04 20:13:14 +0200

    Add into opinionslider.js comments

--- opinionslider.js
+++ opinionslider.js
@@ -1,20 +1,27 @@
 const opinionList = document.querySelectorAll('.opinion');
-const opinionsNumber = opinionList.length;
+const opinionsNumber = opinionList.length; // getting numbers of opinions
 const arrowUp = document.querySelector('.fa-chevron-up');
 const arrowDown = document.querySelector('.fa-chevron-down');
 
 const showOpinion = (opinionToShow, opinionToHide) => {
+
+  // checking if the opinion is first or last
   if (opinionToShow < 0 || opinionToShow >= opinionsNumber) return;
 
+  // show necessary opinion
   opinionList[opinionToShow].classList.add('show-opinion');
+
+  // hide previous opinion
   opinionList[opinionToHide].classList.remove('show-opinion');
 
+  // if first opinion then block arrow-up
   if (opinionToShow === 0) {
     arrowUp.classList.add('blocked-arrow')
   } else {
     arrowUp.classList.remove('blocked-arrow');
   }
 
+  // if last opinion then block arrow-down
   if (opinionToShow === opinionsNumber - 1) {
     arrowDown.classList.add('blocked-arrow')
   } else {
@@ -24,16 +31,22 @@ const showOpinion = (opinionToShow, opinionToHide) => {
 }
 
 const arrowUpFunc = () => {
+  // finding the index of actual visible opinion
   const index = Array.from(opinionList).findIndex(el => {
     return el.classList.contains('show-opinion')
-  })
+  });
+
+  // calling of function showOpinion to show previous opinion and hide the actual
   showOpinion(index - 1, index);
 }
 
 const arrowDownFunc = () => {
+  // finding the index of actual visible opinion
   const index = Array.from(opinionList).findIndex(el => {
     return el.classList.contains('show-opinion')
-  })
+  });
+
+  // calling of function showOpinion to show next opinion and hide the actual
   showOpinion(index + 1, index);
 }
 
